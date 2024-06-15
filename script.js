20efefcb655f4ee38faecde4c95b4a251af4ab27
Suhas <snani4732@gmail.com>
2024-06-15 23:59:41 +0100

    updated css

--- script.js
+++ script.js
@@ -1,3 +1,72 @@
-getElementById("btn-del") = () => {
-    
+// let output = document.getElementById("output-inner")
+let output = document.getElementById("output-inner");
+let currentVal = "";
+let prevVal = "";
+let operator = "";
+
+document.querySelectorAll(".num-inner").forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let buttonText = e.target.textContent;
+
+    if (buttonText === "=") {
+      calculate();
+    } else if (buttonText === "RESET") {
+      reset();
+    } else if (buttonText === "Del") {
+      deleteLastChar();
+    } else if (isOperator(buttonText)) {
+      setOperator(buttonText);
+    } else {
+      appendToOutput(buttonText);
+    }
+  });
+});
+
+function appendToOutput(val) {
+  currentVal += val;
+  output.value = currentVal;
+}
+
+function setOperator(op) {
+  prevVal = currentVal;
+  currentVal = "";
+  operator = op;
+}
+
+function calculate() {
+  let result = 0;
+
+  switch (operator) {
+    case "+":
+      result = parseFloat(prevVal) + parseFloat(currentVal);
+      break;
+    case "-":
+      result = parseFloat(prevVal) - parseFloat(currentVal);
+      break;
+    case "x":
+      result = parseFloat(prevVal) * parseFloat(currentVal);
+      break;
+    case "/":
+      result = parseFloat(prevVal) / parseFloat(currentVal);
+      break;
+  }
+
+  currentVal = result.toString();
+  output.value = currentVal;
+}
+
+function reset() {
+  currentVal = "";
+  prevVal = "";
+  operator = "";
+  output.value = currentVal;
+}
+
+function deleteLastChar() {
+  currentVal = currentVal.slice(0, -1);
+  output.value = currentVal;
+}
+
+function isOperator(val) {
+  return val === "+" || val === "-" || val === "x" || val === "/";
 }
